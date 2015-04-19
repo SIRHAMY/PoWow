083b3c2a2532956cf1aedba32ec32e611c94d4ff
McGarry, Brendan Dennis <bdmcgarry@gatech.edu>
2015-04-19 07:49:49 -0400

    upvote/downvote buttons, post selection js

---
+++ js/post_clicked.js
@@ -0,0 +1,28 @@
+/**
+ * This javascript is triggered when a user clicks on a post to get more info
+ *
+ * created: 04/18/2015
+ * author:  Brendan McGarry
+ */
+ 
+$(document).ready(function(){
+    $(".post").each(
+        function(){
+            $(this).click(postClicked);
+        }//end function
+    );
+});
+
+function postClicked(event){
+    debugln("BEGIN postClicked");
+    var clickedId = event.target.id;
+    debugln("  clickedId: [" + clickedId + "]");
+    debugln("  hiding content areas...");
+    $("#posts").hide();
+    $("#login").hide();
+    $("#profile").hide();
+    $("#ask").hide();
+    $("#post").show(250);
+    //TODO(ALL): use AJAX to load this data and comment section
+    debugln("END postClicked");
+}//end function
