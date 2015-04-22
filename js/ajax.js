b70f82308255a77f62264ad7ee9681bc06167876
McGarry, Brendan Dennis <bdmcgarry@gatech.edu>
2015-04-22 16:33:10 -0400

    Register and PostComment working now!

--- js/ajax.js
+++ js/ajax.js
@@ -111,36 +111,58 @@ function login(event){
     //alert("password: [" + password + "]");
     debugln("  password: [" + pass + "]");
     debugln("  attempting to login...");
+    var submitBtn = $(this).find("input[type=submit]:focus");
+    alert(submitBtn.attr("id"));
     alert("username: [" + userName + "]\npassword: [" + pass + "]");
-    $.ajax({
-        type: "GET",
-        url: "http://default-environment-q4vew696kb.elasticbeanstalk.com/getUser.php",
-        data: {username: userName, password: pass},
-        dataType: 'JSON',
-        success: function(json_data, textStatus, jqXHR){
-            alert("login sent! [" + textStatus + "]");
-            debugln("  found [" + json_data.length + "]  object");
-            debugln("  adding new post...");
-            $.each(json_data, function(key, val){
-                debugln("  " + val.login);
-                if(val.login == "true"){
-                    USER_ID = userName;
-                    alert("LOGGED IN! :D");
-                    //TODO(Brendan): redirect and clear form
-                }//end if
-                else{
-                    USER_ID = "Anonymous";
-                    alert("LOGIN FAILED! D:<");
-                }//end else
-            });
-        }/*,//end function
-        fail: function(msg){
-            alert("FAILURE");
-        },//end function
-        complete: function(msg){
-            alert("COMPLETE");
-        }//end function*/
-    });
+    if(submitBtn.attr("id") == "login-form-submit"){
+        $.ajax({
+            type: "GET",
+            url: "http://default-environment-q4vew696kb.elasticbeanstalk.com/getUser.php",
+            data: {username: userName, password: pass},
+            dataType: 'JSON',
+            success: function(json_data, textStatus, jqXHR){
+                alert("login sent! [" + textStatus + "]");
+                debugln("  found [" + json_data.length + "]  object");
+                debugln("  logging into account...");
+                $.each(json_data, function(key, val){
+                    debugln("  " + val.login);
+                    if(val.login == "true"){
+                        USER_ID = userName;
+                        alert("LOGGED IN! :D");
+                        //TODO(Brendan): redirect and clear form
+                    }//end if
+                    else{
+                        USER_ID = "Anonymous";
+                        alert("LOGIN FAILED! D:<");
+                    }//end else
+                });
+            }//end function
+        });
+    }//end if
+    else if(submitBtn.attr("id") == "login-form-register"){
+        //alert("username: [" + userName + "] password: [" + pass + "]");
+        $.ajax({
+            type: "GET",
+            url: "http://default-environment-q4vew696kb.elasticbeanstalk.com/pushUser.php",
+            data: {username: userName, password: pass},
+            dataType: 'JSON',
+            success: function(json_data, textStatus, jqXHR){
+                alert("register sent! [" + textStatus + "]");
+                debugln("  found [" + json_data.length + "]  object");
+                debugln("  registering account...");
+                $.each(json_data, function(key, val){
+                    debugln("  " + val.joined);
+                    if(val.joined == "true"){
+                        alert("REGISTERED! :D");
+                        //TODO(Brendan): redirect and clear form
+                    }//end if
+                    else{
+                        alert("REGISTRATION FAILED! D:<");
+                    }//end else
+                });
+            }//end function
+        });
+    }//end else if
     debugln("END login");
     //?
     event.preventDefault();
@@ -152,31 +174,28 @@ function postAnswer(event){
     debugln("  getting user name...");
     var userName = USER_ID;
     debugln("  username: [" + userName + "]");
-    debugln("  getting category...");
-    var cat = $("form input[type='radio']:checked").val();
-    debugln("  category: [" + cat + "]");
-    debugln("  getting question title...");
-    var ques = $("#ask-form-question").val();
-    debugln("  password: [" + ques + "]");
-    debugln("  getting more info...");
-    var quesMore = $("#ask-form-more").val();
-    debugln("  more: [" + quesMore + "]");
+    debugln("  getting post id...");
+    var postID = $("#post-id").val();
+    debugln("  postID: [" + postID + "]");
+    debugln("  getting comment...");
+    var comment = $("#post-comment-form-val").val();
+    debugln("  comment: [" + comment + "]");
     debugln("  attempting to post question...");
-    alert("username: [" + userName + "]\ncategory: [" + cat + "]\nquestion: [" + ques + "]\nMore: [" + quesMore + "]");
+    alert("username: [" + userName + "]\npostID: [" + postID + "]\ncomment: [" + comment + "]");
     $.ajax({
         type: "GET",
-        url: "http://default-environment-q4vew696kb.elasticbeanstalk.com/pushQuestion.php",
-        data: {askerID: userName, qCategory: cat, qText: ques, qDescription: quesMore},
+        url: "http://default-environment-q4vew696kb.elasticbeanstalk.com/pushResponse.php",
+        data: {responderID: userName, origQuestion: postID, responseText: comment},
         dataType: 'JSON',
         success: function(json_data, textStatus, jqXHR){
-            alert("question sent! [" + textStatus + "]");
+            alert("comment sent! [" + textStatus + "]");
             debugln("  found [" + json_data.length + "]  object");
-            debugln("  adding new post...");
+            debugln("  adding new comment...");
             $.each(json_data, function(key, val){
                 debugln("  " + val.posted);
                 if(val.posted == "true"){
                     alert("POSTED! :D");
-                    //TODO(Brendan): clear form and redirect
+                    //TODO(Brendan): clear form, refresh page
                 }//end if
                 else{
                     alert("POST FAILED! D:<");
@@ -244,6 +263,7 @@ function showComments(id){
     var categories;
     debugln("  clearing old post...");
     $("#post-comment-section").empty();
+    /*
     debugln("  clearing old comment...");
     $("#post-comment").empty();
     debugln("  adding answer form...");
@@ -252,8 +272,12 @@ function showComments(id){
         "<span style=\"font-size: 10px;\">Your Answer:</span>" +
         "<textarea id=\"post-comment-form-val\"></textarea>" +
         "<input id=\"post-comment-form-submit\" type=\"submit\" value=\"Answer\" />" +
+        "<input type=\"hidden\" id=\"post-id\" value=\"" + id + "\"/>" + 
         "</form>"
     );
+    */
+    debugln("  setting question id");
+    $("#post-id").attr("value", id);
     debugln("  getting AJAX for post ID [" + id + "]...");
     $.ajax({
         type: "GET",
