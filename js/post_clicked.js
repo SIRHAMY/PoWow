a6098acdf19678116fc84217c55cfa9f4a2aad63
McGarry, Brendan Dennis <bdmcgarry@gatech.edu>
2015-04-24 21:45:34 -0400

    fixed bug with clicking pin inside question div
    clicking the pin icon no longer jumps to the thread (which was returning
    undefined).  the event is killed and the thread is not displayed

--- js/post_clicked.js
+++ js/post_clicked.js
@@ -12,12 +12,14 @@ function postClicked(event){
     if(clickedClass == "post-metadata"){
         clickedId = event.target.parentElement.id;
     }//end if
-    //alert("id: [" + clickedId + "]\nclass: [" + clickedClass + "]");
-    debugln("  clickedId: [" + clickedId + "]");
-    debugln("  hiding content areas...");
-    hideContent();
-    showPost(clickedId);
-    showComments(clickedId);
-    $("#post").show(250);
+    //alert("id: [" + clickedId + "]\nclass: [" + clickedClass + "]\nid==\"\": [" + (clickedId == "") + "]");
+    if(clickedId != ""){
+        debugln("  clickedId: [" + clickedId + "]");
+        debugln("  hiding content areas...");
+        hideContent();
+        showPost(clickedId);
+        showComments(clickedId);
+        $("#post").show(250);
+    }//end if
     debugln("END postClicked");
 }//end function   
